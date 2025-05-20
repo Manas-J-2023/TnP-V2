18d60c42d23fc7cec51737ebc365d93c960bb8d3
Manas Kumar <141910018+manaskumar3003@users.noreply.github.com>
2025-05-20 08:21:59 +0530

    added pastrecruiters

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import ForRecruiters from "./pages/For-Recruiters";
 import Contact from "./components/contact/Contact";
+import Pastrecruiters from "./pages/pastrecruiters";
 
 const queryClient = new QueryClient();
 
@@ -34,17 +35,19 @@ const App = () => {
         ) : (
           <BrowserRouter>
             <ScrollToTop />
-            <Navigation /> 
+            <Navigation />
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/about" element={<About />} />
               <Route path="/process" element={<Process />} />
               <Route path="/for-recruiters" element={<ForRecruiters />} />
+              <Route path="/past-recruiters" element={<Pastrecruiters />} />
+
               <Route path="/contact" element={<Contact />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-            <Footer /> 
+            <Footer />
           </BrowserRouter>
         )}
       </TooltipProvider>
